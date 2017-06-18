d1f61b047898ae3b2fa69a83a80f00d50237f958
Andrey Batulov <batulov@gmail.com>
2017-06-19 01:21:42 +0300

    added class StoryAdapterRecycler and xml number_list_item (for future recyclerview)

---
+++ app/src/main/java/com/github/batulovandrey/horrorstories/StoryAdapterRecycler.java
@@ -0,0 +1,68 @@
+package com.github.batulovandrey.horrorstories;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * @author Batulov Andrey on 19.06.17.
+ */
+
+public class StoryAdapterRecycler extends RecyclerView.Adapter<StoryAdapterRecycler.NumberViewHolder> {
+    private final ListItemClickListener mOnClickListener;
+    private int mNumberItems;
+
+    public StoryAdapterRecycler(int numberItems, ListItemClickListener onClickListener) {
+        mNumberItems = numberItems;
+        mOnClickListener = onClickListener;
+    }
+
+    @Override
+    public NumberViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(NumberViewHolder holder, int position) {
+        holder.bind(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mNumberItems;
+    }
+
+    class NumberViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView listItemStoryName;
+        TextView listItemStoryText;
+
+        public NumberViewHolder(View itemView) {
+            super(itemView);
+
+            listItemStoryName = (TextView) itemView.findViewById(R.id.list_item_story_name);
+            listItemStoryText = (TextView) itemView.findViewById(R.id.list_item_story_text);
+            itemView.setOnClickListener(this);
+        }
+
+
+        @Override
+        public void onClick(View v) {
+            int clickedPosition = getAdapterPosition();
+            mOnClickListener.onListItemClick(clickedPosition);
+        }
+
+        void bind(int indexOfStory) {
+            Story story = StoryAdapter.getStoryById(indexOfStory);
+
+            listItemStoryName.setText(story.getName());
+            listItemStoryText.setText(story.getText());
+        }
+    }
+
+    public interface ListItemClickListener {
+        void onListItemClick(int clickedItemIndex);
+    }
+}
